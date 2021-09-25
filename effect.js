45d7ece34a8ce7496daad4efe59f003efc03d589
Moh. Restu Aji Subangkit <44827626+subangkitaji@users.noreply.github.com>
2021-09-25 07:03:06 +0700

    Update effect.js

--- effect.js
+++ effect.js
@@ -161,15 +161,32 @@ $('document').ready(function(){
 				alert("dikit lagi");
 				alert("yaudah yuk sekarang wkwk");
 				$('.message').fadeIn('slow');
+				var i;
+
+				function msgLoop (i) {
+					$("p:nth-child("+i+")").fadeOut('slow').delay(800).promise().done(function(){
+					i=i+1;
+					$("p:nth-child("+i+")").fadeIn('slow').delay(1000);
+					if(i==50){
+				$("p:nth-child(49)").fadeOut('slow').promise().done(function () {
+					$('.cake').fadeIn('fast');
+				});
 				
-				var audio = $('.song')[0];
-				audio.play();
 			}
 			else{
-				confirm("nyesel luh :p");
+				msgLoop(i);
 			}			
 
+		});
+			// body...
+				}
 		
+				msgLoop(0);
+				var audio = $('.song')[0];
+				audio.play();
+				} else  {
+					confirm("nyesel luh :p");
+				}
 		
 			
 		});
